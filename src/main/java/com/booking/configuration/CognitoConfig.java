114a739da54b1da44a7ccacbf6321549ba259a24
Leapfrog <leapfrog@pop-os.localdomain>
2022-03-27 14:07:57 +0545

    [master]: Fixed security issue

--- src/main/java/com/booking/configuration/CognitoConfig.java
+++ src/main/java/com/booking/configuration/CognitoConfig.java
@@ -8,14 +8,21 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CognitoConfig {
+
+    @Value("${aws.cognito.user.accessToken}")
+    private String AWS_ACCESS_TOKEN;
+
+    @Value("${aws.cognito.user.SecretKey}")
+    private String AWS_CLIENT_TOKEN;
     @Bean
     public AWSCognitoIdentityProvider getCognitoClient(){
-        BasicAWSCredentials cred = new BasicAWSCredentials("AKIA5X3F76YEGFBGWX4O","ZQ48KmroSNpVKbbq91Z05tDeL5FzB+tItTx2mNi0");
+        BasicAWSCredentials cred = new BasicAWSCredentials(AWS_ACCESS_TOKEN,AWS_CLIENT_TOKEN);
         AWSCredentialsProvider provider =  new AWSStaticCredentialsProvider(cred);
         return AWSCognitoIdentityProviderClient.builder().standard().withCredentials(provider).withRegion(Regions.AP_SOUTH_1).build();
     }
